4bae785a9ee3af73082bad7384f4bde23f7df94c
David Dume <dumedavid11@gmail.com>
2023-06-22 18:34:11 +0200

    creazione componente home

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script>
+    import Home from './components/Home.vue';
 
+    export default {
+        components: {
+            Home
+        }
+    }
+    
 </script>
 
 <template>
-  
+  <Home></Home>
 </template>
 
 <style >
