0ef177c9da7c6ce0b69b0ad17d5d7223f8f88fd0
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-06-25 17:53:32 +0200

    Grafica category

--- src/pages/DishList.vue
+++ src/pages/DishList.vue
@@ -1,31 +1,57 @@
 <template>
-    <h1 class="text-center mb-3">Menu</h1>
-    <div class="d-flex mx-3 flex-wrap gap-3">
-        <div class="card" style="width: 30rem;" v-for="(dish, index) in dishes" :key="index">
-        <img class="card-img-top h-50" :src="` http://localhost:8000/storage/${dish.image}`" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">{{ dish.name }}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
+    <h1 class="text-center my-3">Menu</h1>
+    
+   <div class="container my-3 me-auto">
+    
+        <div class="d-flex justify-content-center flex-wrap gap-3 my-4">
+            <div class="dish-box m-3 pb-2" v-for="(dish, index) in dishes" :key="index">
+                <div class="dish-img d-flex justify-content-center mt-3">
+                    <img class="rounded "  :src="`http://localhost:8000/storage/${dish.image}`" alt="Card image cap">
+                </div>
+                <div class="px-3">
+                    <p class="text-center px-4 overflow-x-hidden">{{ dish.name }}</p>
+                    <p class="text-center">{{ dish.price }}</p>
+                    <div>
+                        <p class="text-center">Quantità</p>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <div class="p-2 border border-dark rounded m-2" @click="removeQuantity(dish.id)"><i class="fa-solid fa-minus"></i></div>
+                            <div>{{ quantity[index].quantity }}</div>
+                            <div class="p-2 border border-dark rounded m-2" @click="addQuantity(dish.id)"><i class="fa-solid fa-plus"></i></div>
+                            <button @click="addToCart(dish.id)" class="ms_btn">
+                                <i class="fa-solid fa-cart-plus"></i>
+                            </button>                       
+                        </div>
+                   </div>
 
-            <div>
-                <span>Quantità</span>
-                <div class="d-flex align-items-center ">
-                    <div class="p-2 border border-dark rounded m-2" @click="removeQuantity(dish.id)"><i class="fa-solid fa-minus"></i></div>
-                    <div>{{ quantity[index].quantity }}</div>
-                    <div class="p-2 border border-dark rounded m-2" @click="addQuantity(dish.id)"><i class="fa-solid fa-plus"></i></div>
-                    
+                </div>
+            </div>
+            
+        </div>
+    
+    <!-- <div class="d-flex mx-3 flex-wrap gap-3 ">
+        <div class="card" style="width: 18rem;" v-for="(dish, index) in dishes" :key="index">
+            <img class="card-img-top h-50" :src="`http://localhost:8000/storage/${dish.image}`" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">{{ dish.name }}</h5>
+                <p class="card-text">{{ dish.description }}</p>
+                <div>
+                    <span>Quantità</span>
+                    <div class="d-flex align-items-center ">
+                        <div class="p-2 border border-dark rounded m-2" @click="removeQuantity(dish.id)"><i class="fa-solid fa-minus"></i></div>
+                        <div>{{ quantity[index].quantity }}</div>
+                        <div class="p-2 border border-dark rounded m-2" @click="addQuantity(dish.id)"><i class="fa-solid fa-plus"></i></div>
+                        <button @click="addToCart(dish.id)" class="ms_btn">
+                            <i class="fa-solid fa-cart-plus"></i>
+                        </button>                       
+                    </div>
                 </div>
             
-               
             </div>
-            <button @click="addToCart(dish.id)" class="ms_btn">
-                <i class="fa-solid fa-cart-plus"></i>
-            </button>
-        </div>
-    </div>
-
-    </div>
+         </div>
+    </div> -->
+    <div class="d-flex justify-content-center"><a href="" class="ms_btn my-3">Torna alla Home</a></div>
+    
+   </div>
    
 </template> 
 
@@ -129,15 +155,44 @@ export default {
 @use '../style/general.scss';
 @import '../style/partials/_variables.scss';
 
+
+.dish-box{
+    border-radius: 30px;
+    background: rgb(234, 231, 231);
+    transition: 0.8s cubic-bezier(0.22, 0.78, 0.45, 1.02);
+   
+    min-width: 250px;
+        &:hover{
+            transform: scale(1.03);
+            
+        }
+}
+.dish-img{
+            
+            margin-bottom: 30px;
+
+            img{
+                overflow: hidden;
+                max-width: 200px;
+                max-height: 200px;
+                width: 100%;
+                &:hover{
+                    transform: scale(1.02);
+                }
+            }
+        }
 .ms_btn{
     background-color: $bg-secondary;
     color: white;
     padding: 10px;
     border-radius: 10px;
     border: 0px;
+    text-decoration: none;
+    margin-left: 10px;
+    
 }
 .ms_btn:hover{
-    background-color: rgb(174, 153, 153);
+    background-color: $bg-primary;
 }
 
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,24 +3,59 @@
     <AppHeader></AppHeader>
     <AppJumbo></AppJumbo>
     <SingleCategoryCard></SingleCategoryCard>
-    
-    <div v-if="categoriesLoaded" class="category-container" >
-        <div class="category-card" v-for="(category, i) in categories"  @click="selectCategories(category.id)">{{ category.name }}</div>
+
+    <h3 class="text-center my-3">SCEGLI LA CATEGORIA</h3>
+    <div class="container" >
+            <fieldset class="checkbox-group">
+                
+                <div class="checkbox" v-for="(category, i) in categories">
+                    <label class="checkbox-wrapper">
+                        <input type="checkbox" class="checkbox-input" value="category.name" @click="selectCategories(category.id)" />
+                        <span class="checkbox-tile">
+                            <span class="checkbox-icon">
+                                <img :src="`src/assets/${category.slug}.svg`" alt="">
+                            </span>
+                            <span class="checkbox-label">{{ category.name }}</span>
+                        </span>
+                    </label>
+                </div>
+            </fieldset>
+        <div class="d-flex justify-content-center">
+            <button @click="searchCategories(selected)" class=" ms_btn my-3">
+            Cerca Risorante
+            </button>
+
+        </div> 
     </div>
-    <button @click="searchCategories(selected)" class=" ms_btn">
-        Cerca
-    </button>
+   
+    <!-- <div v-if="categoriesLoaded" class="category-container" >
+        <div class="category-card" v-for="(category, i) in categories"><h5>{{ category.name }}</h5> <input class="form-check-input text-center" type="checkbox" value="category.name" id="flexCheckIndeterminate"  @click="selectCategories(category.id)"></div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <button @click="searchCategories(selected)" class=" ms_btn my-3">
+        Cerca Risorante
+        </button>
+
+    </div> -->
+    
     <div v-if="selectedRestaurants">
-        <div v-for="(restaurant, index) in  restaurants " :key="index">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" :src="restaurant.image" alt="Card image cap">
-                <div class="card-body">
-                    <h3 class="card-title">{{ restaurant.name }}</h3>
-                    <h5></h5>
-                    <p class="card-text">{{ restaurant.description }}</p>
-                    <router-link :to="{ name: 'dish-list', params: { id: restaurant.id } }">Menú</router-link>
+        <div class="container mb-5">
+            <div class="row g-4 my-2 d-flex justify-content-center flex-wrap">
+                <h3 class="text-center">RISTORANTI</h3>
+                <div class="col-lg-3 col-md-6" v-for="(restaurant, index) in  restaurants " :key="index">
+                    <div class="restaurant-item text-center">
+                        <div class="my-3">
+                            <img class="img-fluid my-img pt-2" :src="restaurant.image" alt="Card image cap">
+                        </div>
+                        <h3>{{ restaurant.name }}</h3>
+                        <p>{{ restaurant.description }}</p>
+                        <router-link :to="{ name: 'dish-list', params: { id: restaurant.id } }"><button class="ms_btn">Menú</button></router-link>
+
+                    </div>
+
                 </div>
             </div>
+
         </div>
     </div>
 </template>
@@ -40,6 +75,7 @@
                 SingleCategoryCard
                     },
                     data() {
+
         return {
             store,
             categories: '',
@@ -105,31 +141,155 @@
 <style scoped lang="scss">
 @use '../style/general.scss';
 @import '../style/partials/_variables.scss';
-    .category-container {
-        display: flex;
-        justify-content: center;
-        .category-card {
-            padding: 10px;
-            border: 1px solid black;
-            margin: 5px;
-            &active{
-                color: $bg-secondary;
-            }
-            &:hover {
-                cursor: pointer;
-                background-color: $bg-secondary;
-            }
-        
-        }
-       
-       
-    }
+ 
 .ms_btn{
         background-color: $bg-primary;
         color: white;
         padding: 10px;
         border-radius: 10px;
+        border: none;
+        &:hover {
+                background-color: $bg-secondary;
+            }
         
 }
+
+
+.restaurant-item{
+    box-shadow: 0 0 10px rgb( 0, 0, 0.08);
+    height: calc(100% - 3px);
+    transition: 0.5s;
+}
+.my-img{
+        border-radius: 10px;
+        transition: 0.5s;
+        max-width: 90%;
+    }
+    .my-img:hover{
+            transform: scale(1.1);
+        }
+
+
+
+.checkbox-group {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	width: 90%;
+	margin-left: auto;
+	margin-right: auto;
+	max-width: 600px;
+	user-select: none;
+	& > * {
+		margin: .5rem 0.5rem;
+	}
+}
+
+
+
+.checkbox-group-legend {
+	font-size: 1.5rem;
+	font-weight: 700;
+	text-align: center;
+	line-height: 1.125;
+	margin-bottom: 1.25rem;
+}
+
+.checkbox-input {
+	// Code to hide the input
+	clip: rect(0 0 0 0);
+	clip-path: inset(100%);
+	height: 1px;
+	overflow: hidden;
+	position: absolute;
+	white-space: nowrap;
+	width: 1px;
+
+	&:checked + .checkbox-tile {
+		border-color: $bg-primary;
+		box-shadow: 0 5px 10px rgba(#000, 0.1);
+		color: $bg-primary;
+		&:before {
+			transform: scale(1);
+			opacity: 1;
+			background-color: $bg-primary;
+			border-color: $bg-primary;
+		}
+		
+		.checkbox-icon, .checkbox-label {
+			color: $bg-primary;
+		}
+	}
+	
+	&:focus + .checkbox-tile {
+		border-color: $bg-primary;
+		box-shadow: 0 5px 10px rgba(#000, 0.1), 0 0 0 4px $bg-secondary;
+		&:before {
+			transform: scale(1);
+			opacity: 1;
+		}
+	}
+}
+
+.checkbox-tile {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 7rem;
+	min-height: 7rem;
+	border-radius: 0.5rem;
+	border: 2px solid;
+	background-color: #fff;
+	box-shadow: 0 5px 10px rgba(#000, 0.1);
+	transition: 0.15s ease;
+	cursor: pointer;
+	position: relative;
+
+	&:before {
+		content: "";
+		position: absolute;
+		display: block;
+		width: 1.25rem;
+		height: 1.25rem;
+		border: 2px solid;
+		background-color: #fff;
+		border-radius: 50%;
+		top: 0.25rem;
+		left: 0.25rem;
+		opacity: 0;
+		transform: scale(0);
+		transition: 0.25s ease;
+		background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='192' height='192' fill='%23FFFFFF' viewBox='0 0 256 256'%3E%3Crect width='256' height='256' fill='none'%3E%3C/rect%3E%3Cpolyline points='216 72.005 104 184 48 128.005' fill='none' stroke='%23FFFFFF' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'%3E%3C/polyline%3E%3C/svg%3E");
+		background-size: 12px;
+		background-repeat: no-repeat;
+		background-position: 50% 50%;
+	}
+
+	&:hover {
+		border-color: $bg-primary;
+		&:before {
+			transform: scale(1);
+			opacity: 1;
+		}
+	}
+}
+
+.checkbox-icon {
+	color: $bg-primary;
+	img {
+		width: 3rem;
+		height: 3rem;
+	}
+}
+
+.checkbox-label {
+	color: black;
+	transition: .375s ease;
+	text-align: center;
+}
+
+    
+
 
 </style>
