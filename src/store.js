a61292edd74692000bd8cd2816a889e77c0aca9d
David Dume <dumedavid11@gmail.com>
2023-06-29 16:50:18 +0200

    bugfix isEmpty

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { reactive } from 'vue'
 export const store = reactive({
     baseUrl: 'http://localhost:8000/api',
     restaurantId: '',
-    isEmpty: {...localStorage} ? false : true,
+    isEmpty: Object.keys({...localStorage}).length > 0 ? false : true,
     order: [],
     dishes: [],
     showModal: false
