83f6e31e54cbb100c62447e4486ffe72446def36
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-06-30 17:55:29 +0200

    aggiunta sezione best restaurant

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,6 +1,63 @@
 <template>
+    <div class="container">
+        <h1 class="text-center py-4">I ristoranti consigliati</h1>
+        <div class=" d-flex flex-wrap justify-content-center">
+            
+            <div class="restaurant-card m-4"  v-for="restaurant in randomRestaurants">
+                <div class="w-100 d-flex justify-content-center flex-wrap">
+                    <img :src="restaurant.image" alt="" class="background-image">
+                </div>
+                <div class="text-center pt-2">
+                    <span class="fw-bold">{{ restaurant.name }}</span>
+                </div>
+            
+            </div>
+            
+        </div>
+
+    </div>
+    
+
+                
+   
 
 </template>
+
+<style lang="scss" scoped>
+@use '../style/general.scss';
+@import '../style/partials/_variables.scss';
+
+
+.background-image {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  width: 200px;
+  height: 200px;
+  border-radius: 100%;
+  background-color: $bg-secondary;
+}
+
+.restaurant-card {
+        border-radius: 10px;
+        cursor: pointer;
+        max-height: 240px;
+        width: 200px;
+        transition: transform 250ms;
+        margin: 0 10px;
+        &:hover {
+          transform: translateY(-5px);
+         
+        }}
+          
+                     
+  
+            
+          
+
+
+
+</style>
 
 
 <script>
@@ -19,7 +76,7 @@
         },
         methods: {
             randRestaurants() {
-                for(let i = 0; i < 5; i++) {
+                for(let i = 0; i < 4; i++) {
                     let index = Math.floor(Math.random() * this.restaurants.length);
                     if(!this.randomRestaurants.some(el => el.id == index)) {
                             this.randomRestaurants.push({
