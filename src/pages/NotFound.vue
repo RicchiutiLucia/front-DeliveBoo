b6320b71be76e920b6ba25731984af357c6c6d7a
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-07-04 08:52:17 +0200

    404

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -5,9 +5,15 @@
                 <img class="mb-4 w-100 w-md-75" src="../../../../public/404.png" alt="404">
             </div>
         </div>
-        <div class="text-error d-flex">
+        <div class="text-error">
             <h2 class="text-white col-sm-8 font-weight-bold">"Oops, pagina non trovata. </h2>
+            <h4><router-link class="ms-btn text-decoration-none p-1 mt-3"  :to="{ name: 'home' }">Torna alla Home</router-link> </h4>
+
         </div>
+        <div>
+            
+        </div>
+        
     </div>
 </template>
 
@@ -19,7 +25,8 @@ export default {
 
 <style scoped lang="scss">
 
-
+@use '../style/general.scss';
+@import '../style/partials/_variables.scss';
 .error{
     background-image: url(../../../../public/error.png);
     height: 100vh;
@@ -43,9 +50,21 @@ export default {
     position: absolute;
     left: 85%;
     top: 50%;
-    transform: translate(-50%, 25%);
+    transform: translate(-55%, 25%);
     text-shadow: 0px 7px 8px rgba(33, 33, 33, 0.3);
     width: 85vw;
+}
+.ms-btn {
+    background-color: $bg-primary;
+    color: white;
+    border-radius: 10px;
+
+
+    &:hover {
+        border: 2px solid $bg-secondary;
+        background-color: white;
+        color: black;
+    }
 }
 
 </style>
